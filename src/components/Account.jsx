01d01d01d004b5416ee56e0e915bd96ac1d3d9ae
JonasWalker <jonaswalker99@yahoo.com>
2022-02-09 02:54:36 +0000

    editing account

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,101 +1,122 @@
-import React from 'react'
+import React from 'react';
 
 export const Account = () => (
   <React.Fragment>
     <head>
-      <meta name='theme-color' content='#7952b3'></meta>
-      <link href='signin.css' rel='stylesheet' />
+      <meta name="theme-color" content="#7952b3"></meta>
+      <link href="signin.css" rel="stylesheet" />
       <link
-        rel='icon'
-        href='/BugItLogo.png'
-        sizes='32x32'
-        type='image/png'
+        rel="icon"
+        href="/BugItLogo.png"
+        sizes="32x32"
+        type="image/png"
       ></link>
     </head>
-    <body class='text-center'>
+    <body class="text-center">
       {/* class="form-floating col row g-3" */}
-      <form class='row g-3'>
-        <div class='row'>
-          <div class='col-md-3'>
-            <label for='inputFirstName' class='form-label'>
+      <form class="row g-3">
+        <div class="row">
+          <div class="col-md-2">
+            <label for="inputFirstName" class="form-label">
               First Name
             </label>
             <input
-              type='text'
-              class='form-control'
-              id='inputFirstName'
-              placeholder='John'
+              type="text"
+              class="form-control"
+              id="inputFirstName"
+              placeholder="John"
             />
           </div>
-          <div class='col-md-3'>
-            <label for='inputLastName' class='form-label'>
+          <div class="col-md-2">
+            <label for="inputLastName" class="form-label">
               Last Name
             </label>
             <input
-              type='text'
-              class='form-control'
-              id='inputLastName'
-              placeholder='Doe'
+              type="text"
+              class="form-control"
+              id="inputLastName"
+              placeholder="Doe"
             />
           </div>
         </div>
-        <div class='col-6'>
-          <label for='inputEmail' class='form-label'>
-            Email
-          </label>
-          <input
-            type='email'
-            class='form-control'
-            id='inputAddress'
-            placeholder='name@example.com'
-          />
+        <div class="row">
+          <div class="col-md-2">
+            <label for="inputEmail" class="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="inputAddress"
+              placeholder="name@example.com"
+            />
+          </div>
+          <div class="col-md-2">
+            <label for="inputPhoneNumber" class="form-label">
+              Phone Number
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="inputPhoneNumber"
+              placeholder="(XXX)XXX-XXXX"
+              pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+            />
+          </div>
         </div>
-        <div class='col-12'>
-          <label for='inputAddress2' class='form-label'>
-            Phone Number
-          </label>
-          <input
-            type='text'
-            class='form-control'
-            id='inputAddress2'
-            placeholder='Apartment, studio, or floor'
-          />
+        <div class="row">
+          <div class="col-md-2">
+            <label for="inputCompany" class="form-label">
+              Company
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="inputCompany"
+              placeholder="BugIt"
+            />
+          </div>
+          <div class="col-md-2">
+            <label for="inputPhoneNumber" class="form-label">
+              Phone Number
+            </label>
+            <input
+              type="tel"
+              class="form-control"
+              id="inputPhoneNumber"
+              placeholder="(XXX)XXX-XXXX"
+            />
+          </div>
         </div>
-        <div class='col-md-6'>
-          <label for='inputCity' class='form-label'>
-            City
-          </label>
-          <input type='text' class='form-control' id='inputCity' />
-        </div>
-        <div class='col-md-4'>
-          <label for='inputState' class='form-label'>
+        <div class="col-md-4">
+          <label for="inputState" class="form-label">
             State
           </label>
-          <select id='inputState' class='form-select'>
+          <select id="inputState" class="form-select">
             <option selected>Choose...</option>
             <option>...</option>
           </select>
         </div>
-        <div class='col-md-2'>
-          <label for='inputZip' class='form-label'>
+        <div class="col-md-2">
+          <label for="inputZip" class="form-label">
             Zip
           </label>
-          <input type='text' class='form-control' id='inputZip' />
+          <input type="text" class="form-control" id="inputZip" />
         </div>
-        <div class='col-12'>
-          <div class='form-check'>
-            <input class='form-check-input' type='checkbox' id='gridCheck' />
-            <label class='form-check-label' for='gridCheck'>
+        <div class="col-12">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="gridCheck" />
+            <label class="form-check-label" for="gridCheck">
               Check me out
             </label>
           </div>
         </div>
-        <div class='col-12'>
-          <button type='submit' class='btn btn-primary'>
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary">
             Sign in
           </button>
         </div>
       </form>
     </body>
   </React.Fragment>
-)
+);
