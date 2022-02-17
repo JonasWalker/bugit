ccb5af9e2ff4651a2db55017f79ce756ee74d49f
JonasWalker <67254086+JonasWalker@users.noreply.github.com>
2022-02-16 21:08:32 -0600

    added more to bug table and added buttons. buttons dont work

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,93 @@
-import React from 'react';
+import React from 'react'
 //import BugItLogo from './images/BugItLogo.jpg';
-import './CSS/home.css';
+import './CSS/home.css'
 const players = [
-  { software: 'BugIt', name: 'Jonas', date: 'never' },
-  { software: 'BugIt', name: 'Hunter', date: 'never' },
-  { software: 'BugIt', name: 'Jarod', date: 'never' },
-  { software: 'BugIt', name: 'Ben', date: 'never' },
-];
+  {
+    software: 'BugIt',
+    name: 'Jonas',
+    date: 'never',
+    type: 'Optimize',
+    status: 'In Progress',
+    priority: 'Low',
+    estimatedTime: 'trust',
+  },
+  {
+    software: 'BugIt',
+    name: 'Hunter',
+    date: 'never',
+    type: 'Optimize',
+    status: 'In Progress',
+    priority: 'Low',
+    estimatedTime: 'trust',
+  },
+  {
+    software: 'BugIt',
+    name: 'Jarod',
+    date: 'never',
+    type: 'Optimize',
+    status: 'In Progress',
+    priority: 'Low',
+    estimatedTime: 'trust',
+  },
+  {
+    software: 'BugIt',
+    name: 'Ben',
+    date: 'never',
+    type: 'Optimize',
+    status: 'In Progress',
+    priority: 'Low',
+    estimatedTime: 'trust',
+  },
+]
 
-const renderBug = (bug, index) => {
+function renderBug(bug, index) {
   return (
     <tr key={index}>
       <td>{bug.software}</td>
       <td>{bug.name}</td>
       <td>{bug.date}</td>
+      <td>{bug.type}</td>
+      <td>{bug.status}</td>
+      <td>{bug.priority}</td>
+      <td>{bug.estimatedTime}</td>
     </tr>
-  );
-};
+  )
+}
+
+function addBug() {
+  console.log('added bug')
+}
+
+function deleteBug() {
+  console.log('deleted bug')
+}
 
 export const Home = () => (
   <body>
     <h1>BugIt</h1>
-    <main>
-      <table>
+    <div class='table-responsive'>
+      <table class='table table-striped table-sm'>
         <thead>
           <tr>
             <th>Software</th>
             <th>Name</th>
-            <th>Team</th>
+            <th>Date</th>
+            <th>Type</th>
+            <th>Status</th>
+            <th>Priority</th>
+            <th>Estimated Time</th>
           </tr>
         </thead>
         <tbody>{players.map(renderBug)}</tbody>
       </table>
-    </main>
+    </div>
+    <div>
+      <button type='button' class='btn btn-success' onClick='addBug()'>
+        Add
+      </button>
+      <button type='button' class='btn btn-danger' onClick='deleteBug()'>
+        Delete
+      </button>
+    </div>
   </body>
-);
+)
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,159 +10,147 @@ import './CSS/home.css'
 
 export const Sidebar = () => (
   <React.Fragment>
-    <body>
-      <div class='container-fluid'>
-        <div class='row'>
-          {/* class="d-flex flex-column flex-shrink-0 bg-light" */}
-          <nav
-            id='sidebarMenu'
-            class='col-md-3 col-lg-2 d-md-block bg-light sidebar collapse'
-            style={{ float: 'left' }}
-          >
-            <ul class='nav nav-pills nav-flush flex-column mb-auto text-center'>
-              <li>
-                <a
-                  href='/login'
-                  class='nav-link active py-3 border-bottom'
-                  aria-current='page'
-                  title='login'
-                  data-bs-toggle='tooltip'
-                  data-bs-placement='right'
-                >
-                  Login
-                </a>
-              </li>
-              <li>
-                <a
-                  href='/createAccount'
-                  class='nav-link active py-3 border-bottom'
-                  aria-current='page'
-                  title='signup'
-                  data-bs-toggle='tooltip'
-                  data-bs-placement='right'
-                >
-                  Sign up
-                </a>
-              </li>
-              <li>
-                <a
-                  // href points refer to the destination ex: www.abc.com/home   or.com/about
-                  href='/#home'
-                  class='nav-link py-3 border-bottom'
-                  title='Home'
-                  data-bs-toggle='tooltip'
-                  data-bs-placement='right'
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href='/about'
-                  class='nav-link py-3 border-bottom'
-                  title='About'
-                  data-bs-toggle='tooltip'
-                  data-bs-placement='right'
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href='/contact'
-                  class='nav-link py-3 border-bottom'
-                  title='Contact'
-                  data-bs-toggle='tooltip'
-                  data-bs-placement='right'
-                >
-                  Contact
-                </a>
-              </li>
-              <li>
-                <a
-                  href='/nomatch'
-                  class='nav-link py-3 border-bottom'
-                  title='NoMatch'
-                  data-bs-toggle='tooltip'
-                  data-bs-placement='right'
-                >
-                  {' '}
-                </a>
-              </li>
-              <li>
-                <a
-                  href='/nomatch'
-                  class='nav-link py-3 border-bottom'
-                  title='nomatch'
-                  data-bs-toggle='tooltip'
-                  data-bs-placement='right'
-                >
-                  {' '}
-                </a>
-              </li>
-              <li>
-                <a
-                  href='/nomatch'
-                  class='nav-link py-3 border-bottom'
-                  title='NoMatch'
-                  data-bs-toggle='tooltip'
-                  data-bs-placement='right'
-                >
-                  {' '}
-                </a>
-              </li>
-              <li>
-                <a
-                  href='/nomatch'
-                  class='nav-link py-3 border-bottom'
-                  title='NoMatch'
-                  data-bs-toggle='tooltip'
-                  data-bs-placement='right'
-                >
-                  {' '}
-                </a>
-              </li>
-              <li>
-                <a
-                  href='/nomatch'
-                  class='nav-link py-3 border-bottom'
-                  title='NoMatch'
-                  data-bs-toggle='tooltip'
-                  data-bs-placement='right'
-                >
-                  {' '}
-                </a>
-              </li>
-              <li>
-                <a
-                  href='/account'
-                  class='nav-link py-3 border-bottom'
-                  title='Account'
-                  data-bs-toggle='tooltip'
-                  data-bs-placement='right'
-                >
-                  Account
-                </a>
-              </li>
+    <div class='container-fluid'>
+      <div class='row'>
+        {/* class="d-flex flex-column flex-shrink-0 bg-light" */}
+        <nav
+          id='sidebarMenu'
+          class='col-md-3 col-lg-2 d-md-block bg-light sidebar collapse'
+          style={{ float: 'left' }}
+        >
+          <ul class='nav nav-pills nav-flush flex-column mb-auto text-center'>
+            <li>
               <a
-                href='/account'
-                class='bottomAccount nav-link py-3 border-bottom'
-                title='Account'
+                href='/login'
+                class='nav-link active py-3 border-bottom'
+                aria-current='page'
+                title='login'
                 data-bs-toggle='tooltip'
                 data-bs-placement='right'
               >
-                Account
+                Login
               </a>
-            </ul>
-          </nav>
-          <main class='col-md-9 ms-sm-auto col-lg-10 px-md-4'>
-            {/* An <Outlet> renders whatever child route is currently active,
+            </li>
+            <li>
+              <a
+                href='/createAccount'
+                class='nav-link active py-3 border-bottom'
+                aria-current='page'
+                title='signup'
+                data-bs-toggle='tooltip'
+                data-bs-placement='right'
+              >
+                Sign up
+              </a>
+            </li>
+            <li>
+              <a
+                // href points refer to the destination ex: www.abc.com/home   or.com/about
+                href='/#home'
+                class='nav-link py-3 border-bottom'
+                title='Home'
+                data-bs-toggle='tooltip'
+                data-bs-placement='right'
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href='/about'
+                class='nav-link py-3 border-bottom'
+                title='About'
+                data-bs-toggle='tooltip'
+                data-bs-placement='right'
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href='/contact'
+                class='nav-link py-3 border-bottom'
+                title='Contact'
+                data-bs-toggle='tooltip'
+                data-bs-placement='right'
+              >
+                Contact
+              </a>
+            </li>
+            <li>
+              <a
+                href='/nomatch'
+                class='nav-link py-3 border-bottom'
+                title='NoMatch'
+                data-bs-toggle='tooltip'
+                data-bs-placement='right'
+              >
+                {' '}
+              </a>
+            </li>
+            <li>
+              <a
+                href='/nomatch'
+                class='nav-link py-3 border-bottom'
+                title='nomatch'
+                data-bs-toggle='tooltip'
+                data-bs-placement='right'
+              >
+                {' '}
+              </a>
+            </li>
+            <li>
+              <a
+                href='/nomatch'
+                class='nav-link py-3 border-bottom'
+                title='NoMatch'
+                data-bs-toggle='tooltip'
+                data-bs-placement='right'
+              >
+                {' '}
+              </a>
+            </li>
+            <li>
+              <a
+                href='/nomatch'
+                class='nav-link py-3 border-bottom'
+                title='NoMatch'
+                data-bs-toggle='tooltip'
+                data-bs-placement='right'
+              >
+                {' '}
+              </a>
+            </li>
+            <li>
+              <a
+                href='/nomatch'
+                class='nav-link py-3 border-bottom'
+                title='NoMatch'
+                data-bs-toggle='tooltip'
+                data-bs-placement='right'
+              >
+                {' '}
+              </a>
+            </li>
+            <li></li>
+            <a
+              href='/account'
+              class='nav-link py-3 border-bottom'
+              title='Account'
+              data-bs-toggle='tooltip'
+              data-bs-placement='right'
+            >
+              Account
+            </a>
+          </ul>
+        </nav>
+        <main class='col-md-9 ms-sm-auto col-lg-11'>
+          {/* An <Outlet> renders whatever child route is currently active,
             so you can think about this <Outlet> as a placeholder for
             the child routes we defined above. */}
-            <Outlet />
-          </main>
-        </div>
+          <Outlet />
+        </main>
       </div>
-    </body>
+    </div>
   </React.Fragment>
 )
