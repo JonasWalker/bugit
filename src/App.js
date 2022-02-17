2919e487d337005a1ebe529d889cf4486ff57db7
JonasWalker <67254086+JonasWalker@users.noreply.github.com>
2022-02-16 19:15:09 -0600

    fixed sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
-import Layout from './components/Sidebar'
+import { Sidebar } from './components/Sidebar'
 import { Home } from './components/Home'
 import { About } from './components/About'
 import { Contact } from './components/Contact'
@@ -16,7 +16,7 @@ export default function App() {
       <Routes>
         <Route path='login' element={<Login />} />
         <Route path='createAccount' element={<CreateAccount />} />
-        <Route path='/' element={<Layout />}>
+        <Route path='/' element={<Sidebar />}>
           <Route index element={<Home />} />
           <Route path='about' element={<About />} />
           <Route path='contact' element={<Contact />} />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,166 +1,168 @@
-import React from 'react';
+import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Link,
   Outlet,
-} from 'react-router-dom';
-import './CSS/home.css';
+} from 'react-router-dom'
+import './CSS/home.css'
 
-function Sidebar() {
-  return (
+export const Sidebar = () => (
+  <React.Fragment>
     <body>
-      <main>
-        {/* class="d-flex flex-column flex-shrink-0 bg-light" */}
-        <nav
-          id="sidebarMenu"
-          class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
-          style={{ float: 'left' }}
-        >
-          <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-            <li>
+      <div class='container-fluid'>
+        <div class='row'>
+          {/* class="d-flex flex-column flex-shrink-0 bg-light" */}
+          <nav
+            id='sidebarMenu'
+            class='col-md-3 col-lg-2 d-md-block bg-light sidebar collapse'
+            style={{ float: 'left' }}
+          >
+            <ul class='nav nav-pills nav-flush flex-column mb-auto text-center'>
+              <li>
+                <a
+                  href='/login'
+                  class='nav-link active py-3 border-bottom'
+                  aria-current='page'
+                  title='login'
+                  data-bs-toggle='tooltip'
+                  data-bs-placement='right'
+                >
+                  Login
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/createAccount'
+                  class='nav-link active py-3 border-bottom'
+                  aria-current='page'
+                  title='signup'
+                  data-bs-toggle='tooltip'
+                  data-bs-placement='right'
+                >
+                  Sign up
+                </a>
+              </li>
+              <li>
+                <a
+                  // href points refer to the destination ex: www.abc.com/home   or.com/about
+                  href='/#home'
+                  class='nav-link py-3 border-bottom'
+                  title='Home'
+                  data-bs-toggle='tooltip'
+                  data-bs-placement='right'
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/about'
+                  class='nav-link py-3 border-bottom'
+                  title='About'
+                  data-bs-toggle='tooltip'
+                  data-bs-placement='right'
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/contact'
+                  class='nav-link py-3 border-bottom'
+                  title='Contact'
+                  data-bs-toggle='tooltip'
+                  data-bs-placement='right'
+                >
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/nomatch'
+                  class='nav-link py-3 border-bottom'
+                  title='NoMatch'
+                  data-bs-toggle='tooltip'
+                  data-bs-placement='right'
+                >
+                  {' '}
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/nomatch'
+                  class='nav-link py-3 border-bottom'
+                  title='nomatch'
+                  data-bs-toggle='tooltip'
+                  data-bs-placement='right'
+                >
+                  {' '}
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/nomatch'
+                  class='nav-link py-3 border-bottom'
+                  title='NoMatch'
+                  data-bs-toggle='tooltip'
+                  data-bs-placement='right'
+                >
+                  {' '}
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/nomatch'
+                  class='nav-link py-3 border-bottom'
+                  title='NoMatch'
+                  data-bs-toggle='tooltip'
+                  data-bs-placement='right'
+                >
+                  {' '}
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/nomatch'
+                  class='nav-link py-3 border-bottom'
+                  title='NoMatch'
+                  data-bs-toggle='tooltip'
+                  data-bs-placement='right'
+                >
+                  {' '}
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/account'
+                  class='nav-link py-3 border-bottom'
+                  title='Account'
+                  data-bs-toggle='tooltip'
+                  data-bs-placement='right'
+                >
+                  Account
+                </a>
+              </li>
               <a
-                href="/login"
-                class="nav-link active py-3 border-bottom"
-                aria-current="page"
-                title="login"
-                data-bs-toggle="tooltip"
-                data-bs-placement="right"
-              >
-                Login
-              </a>
-            </li>
-            <li>
-              <a
-                href="/createAccount"
-                class="nav-link active py-3 border-bottom"
-                aria-current="page"
-                title="signup"
-                data-bs-toggle="tooltip"
-                data-bs-placement="right"
-              >
-                Sign up
-              </a>
-            </li>
-            <li>
-              <a
-                // href points refer to the destination ex: www.abc.com/home   or.com/about
-                href="/#home"
-                class="nav-link py-3 border-bottom"
-                title="Home"
-                data-bs-toggle="tooltip"
-                data-bs-placement="right"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="/about"
-                class="nav-link py-3 border-bottom"
-                title="About"
-                data-bs-toggle="tooltip"
-                data-bs-placement="right"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="/contact"
-                class="nav-link py-3 border-bottom"
-                title="Contact"
-                data-bs-toggle="tooltip"
-                data-bs-placement="right"
-              >
-                Contact
-              </a>
-            </li>
-            <li>
-              <a
-                href="/nomatch"
-                class="nav-link py-3 border-bottom"
-                title="NoMatch"
-                data-bs-toggle="tooltip"
-                data-bs-placement="right"
-              >
-                {' '}
-              </a>
-            </li>
-            <li>
-              <a
-                href="/nomatch"
-                class="nav-link py-3 border-bottom"
-                title="nomatch"
-                data-bs-toggle="tooltip"
-                data-bs-placement="right"
-              >
-                {' '}
-              </a>
-            </li>
-            <li>
-              <a
-                href="/nomatch"
-                class="nav-link py-3 border-bottom"
-                title="NoMatch"
-                data-bs-toggle="tooltip"
-                data-bs-placement="right"
-              >
-                {' '}
-              </a>
-            </li>
-            <li>
-              <a
-                href="/nomatch"
-                class="nav-link py-3 border-bottom"
-                title="NoMatch"
-                data-bs-toggle="tooltip"
-                data-bs-placement="right"
-              >
-                {' '}
-              </a>
-            </li>
-            <li>
-              <a
-                href="/nomatch"
-                class="nav-link py-3 border-bottom"
-                title="NoMatch"
-                data-bs-toggle="tooltip"
-                data-bs-placement="right"
-              >
-                {' '}
-              </a>
-            </li>
-            <li>
-              <a
-                href="/account"
-                class="nav-link py-3 border-bottom"
-                title="Account"
-                data-bs-toggle="tooltip"
-                data-bs-placement="right"
+                href='/account'
+                class='bottomAccount nav-link py-3 border-bottom'
+                title='Account'
+                data-bs-toggle='tooltip'
+                data-bs-placement='right'
               >
                 Account
               </a>
-            </li>
-            <a
-              href="/account"
-              class="bottomAccount nav-link py-3 border-bottom"
-              title="Account"
-              data-bs-toggle="tooltip"
-              data-bs-placement="right"
-            >
-              Account
-            </a>
-          </ul>
-        </nav>
-        {/* An <Outlet> renders whatever child route is currently active,
+            </ul>
+          </nav>
+          <main class='col-md-9 ms-sm-auto col-lg-10 px-md-4'>
+            {/* An <Outlet> renders whatever child route is currently active,
             so you can think about this <Outlet> as a placeholder for
             the child routes we defined above. */}
-        <Outlet />
-      </main>
+            <Outlet />
+          </main>
+        </div>
+      </div>
     </body>
-  );
-}
-
-export default Sidebar;
+  </React.Fragment>
+)
