1687ac15326bb545e1410176734e58678e25d2f3
JonasWalker <jonaswalker99@yahoo.com>
2022-03-08 16:50:36 +0000

    added new home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,62 +1,25 @@
-import { render } from '@testing-library/react'
-import React from 'react'
+//import { render } from '@testing-library/react'
+import React, { useState, useEffect } from 'react';
 //import BugItLogo from './images/BugItLogo.jpg';
-import './CSS/home.css'
-const bugsInfo = [
-  {
-    software: 'BugIt',
-    name: 'Jonas',
-    date: 'never',
-    type: 'Optimize',
-    status: 'In Progress',
-    priority: 'Low',
-    estimatedTime: 'trust',
-  },
-  {
-    software: 'BugIt',
-    name: 'Hunter',
-    date: 'never',
-    type: 'Optimize',
-    status: 'In Progress',
-    priority: 'Low',
-    estimatedTime: 'trust',
-  },
-  {
-    software: 'BugIt',
-    name: 'Jarod',
-    date: 'never',
-    type: 'Optimize',
-    status: 'In Progress',
-    priority: 'Low',
-    estimatedTime: 'trust',
-  },
-  {
-    software: 'BugIt',
-    name: 'Ben',
-    date: 'never',
-    type: 'Optimize',
-    status: 'In Progress',
-    priority: 'Low',
-    estimatedTime: 'trust',
-  },
-]
+import './CSS/home.css';
+import Data from './data.json';
 
-function renderBug(bug, index) {
+function readJson(bugs, index) {
   return (
     <tr key={index}>
-      <td>{bug.software}</td>
-      <td>{bug.name}</td>
-      <td>{bug.date}</td>
-      <td>{bug.type}</td>
-      <td>{bug.status}</td>
-      <td>{bug.priority}</td>
-      <td>{bug.estimatedTime}</td>
+      <td>{bugs.software}</td>
+      <td>{bugs.name}</td>
+      <td>{bugs.date}</td>
+      <td>{bugs.type}</td>
+      <td>{bugs.status}</td>
+      <td>{bugs.priority}</td>
+      <td>{bugs.estimatedTime}</td>
     </tr>
-  )
+  );
 }
 
 function addBug() {
-  console.log('added bug')
+  console.log('added bug');
   const newBug = {
     software: 'BugIt',
     name: 'Jonas 2',
@@ -65,28 +28,39 @@ function addBug() {
     status: 'In Progress',
     priority: 'Low',
     estimatedTime: 'trust trust',
-  }
-  document.getElementById('myTable').insertRow(newBug)
-  bugsInfo.push(newBug)
-  document.getElementById('myTbody').insertRow(newBug)
-  renderBug(bugsInfo)
-  console.log(bugsInfo)
-  return
+  };
+  document.getElementById('myTable').insertRow(newBug);
+  document.getElementById('myTbody').insertRow(newBug);
+  return;
 }
 
 function deleteBug() {
-  console.log('deleted bug')
-  bugsInfo.pop()
-  document.getElementById('myTable').deleteRow(1)
-  console.log(bugsInfo)
-  return
+  console.log('deleted bug');
+  document.getElementById('myTable').deleteRow(1);
+  return;
 }
+
+// function showUser() {
+//   const [user, setuser] = useState({ Email: '', Password: '' })
+//   useEffect(() => {
+//     var a = localStorage.getItem('myData')
+//     var b = JSON.parse(a)
+//     console.log(b.Name)
+//     setuser(b)
+//     console.log(user.Name)
+//   }, [])
+//   return (
+//     <>
+//       <div class='bottomleft'>{user.Name}</div>
+//     </>
+//   )
+// }
 
 export const Home = () => (
   <body>
     <h1>BugIt</h1>
-    <div class='table-responsive'>
-      <table class='table table-striped table-sm' id='myTable'>
+    <div class="table-responsive">
+      <table class="table table-striped table-sm" id="myTable">
         <thead>
           <tr>
             <th>Software</th>
@@ -98,16 +72,16 @@ export const Home = () => (
             <th>Estimated Time</th>
           </tr>
         </thead>
-        <tbody id='myTbody'>{bugsInfo.map(renderBug)}</tbody>
+        <tbody id="myTbody">{Data.map(readJson)}</tbody>
       </table>
     </div>
     <div>
-      <button type='button' class='btn btn-success' onClick={addBug}>
+      <button type="button" class="btn btn-success" onClick={addBug}>
         Add
       </button>
-      <button type='button' class='btn btn-danger' onClick={deleteBug}>
+      <button type="button" class="btn btn-danger" onClick={deleteBug}>
         Delete
       </button>
     </div>
   </body>
-)
+);
