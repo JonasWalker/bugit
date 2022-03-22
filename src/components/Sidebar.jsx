ad4f52d0feba58e669132e75107a40cc3bc168f2
JonasWalker <jonaswalker99@yahoo.com>
2022-03-22 13:39:42 +0000

    changed class to className

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,44 +1,44 @@
-import React from 'react'
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Link,
   Outlet,
-} from 'react-router-dom'
-import './CSS/home.css'
+} from 'react-router-dom';
+import './CSS/home.css';
 
 export const Sidebar = () => (
   <React.Fragment>
-    <div class='container-fluid'>
-      <div class='row'>
-        {/* class="d-flex flex-column flex-shrink-0 bg-light" */}
+    <div className="container-fluid">
+      <div className="row">
+        {/* className="d-flex flex-column flex-shrink-0 bg-light" */}
         <nav
-          id='sidebarMenu'
-          class='col-md-3 col-lg-2 d-md-block bg-light sidebar collapse'
+          id="sidebarMenu"
+          className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
           style={{ float: 'left' }}
         >
-          <ul class='nav nav-pills nav-flush flex-column mb-auto text-center'>
+          <ul className="nav nav-pills nav-flush flex-column mb-auto text-center">
             <li>
               <a
-                href='/login'
-                class='nav-link active py-3 border-bottom'
-                aria-current='page'
-                title='login'
-                data-bs-toggle='tooltip'
-                data-bs-placement='right'
+                href="/login"
+                className="nav-link active py-3 border-bottom"
+                aria-current="page"
+                title="login"
+                data-bs-toggle="tooltip"
+                data-bs-placement="right"
               >
                 Login
               </a>
             </li>
             <li>
               <a
-                href='/createAccount'
-                class='nav-link active py-3 border-bottom'
-                aria-current='page'
-                title='signup'
-                data-bs-toggle='tooltip'
-                data-bs-placement='right'
+                href="/createAccount"
+                className="nav-link active py-3 border-bottom"
+                aria-current="page"
+                title="signup"
+                data-bs-toggle="tooltip"
+                data-bs-placement="right"
               >
                 Sign up
               </a>
@@ -46,106 +46,106 @@ export const Sidebar = () => (
             <li>
               <a
                 // href points refer to the destination ex: www.abc.com/home   or.com/about
-                href='/#home'
-                class='nav-link py-3 border-bottom'
-                title='Home'
-                data-bs-toggle='tooltip'
-                data-bs-placement='right'
+                href="/#home"
+                className="nav-link py-3 border-bottom"
+                title="Home"
+                data-bs-toggle="tooltip"
+                data-bs-placement="right"
               >
                 Home
               </a>
             </li>
             <li>
               <a
-                href='/about'
-                class='nav-link py-3 border-bottom'
-                title='About'
-                data-bs-toggle='tooltip'
-                data-bs-placement='right'
+                href="/about"
+                className="nav-link py-3 border-bottom"
+                title="About"
+                data-bs-toggle="tooltip"
+                data-bs-placement="right"
               >
                 About
               </a>
             </li>
             <li>
               <a
-                href='/contact'
-                class='nav-link py-3 border-bottom'
-                title='Contact'
-                data-bs-toggle='tooltip'
-                data-bs-placement='right'
+                href="/contact"
+                className="nav-link py-3 border-bottom"
+                title="Contact"
+                data-bs-toggle="tooltip"
+                data-bs-placement="right"
               >
                 Contact
               </a>
             </li>
             <li>
               <a
-                href='/newCreateAccount'
-                class='nav-link active py-3 border-bottom'
-                aria-current='page'
-                title='signup'
-                data-bs-toggle='tooltip'
-                data-bs-placement='right'
+                href="/newCreateAccount"
+                className="nav-link active py-3 border-bottom"
+                aria-current="page"
+                title="signup"
+                data-bs-toggle="tooltip"
+                data-bs-placement="right"
               >
                 New Sign up
               </a>
             </li>
             <li>
               <a
-                href='/nomatch'
-                class='nav-link py-3 border-bottom'
-                title='nomatch'
-                data-bs-toggle='tooltip'
-                data-bs-placement='right'
+                href="/nomatch"
+                className="nav-link py-3 border-bottom"
+                title="nomatch"
+                data-bs-toggle="tooltip"
+                data-bs-placement="right"
               >
                 {' '}
               </a>
             </li>
             <li>
               <a
-                href='/nomatch'
-                class='nav-link py-3 border-bottom'
-                title='NoMatch'
-                data-bs-toggle='tooltip'
-                data-bs-placement='right'
+                href="/nomatch"
+                className="nav-link py-3 border-bottom"
+                title="NoMatch"
+                data-bs-toggle="tooltip"
+                data-bs-placement="right"
               >
                 {' '}
               </a>
             </li>
             <li>
               <a
-                href='/nomatch'
-                class='nav-link py-3 border-bottom'
-                title='NoMatch'
-                data-bs-toggle='tooltip'
-                data-bs-placement='right'
+                href="/nomatch"
+                className="nav-link py-3 border-bottom"
+                title="NoMatch"
+                data-bs-toggle="tooltip"
+                data-bs-placement="right"
               >
                 {' '}
               </a>
             </li>
             <li>
               <a
-                href='/nomatch'
-                class='nav-link py-3 border-bottom'
-                title='NoMatch'
-                data-bs-toggle='tooltip'
-                data-bs-placement='right'
+                href="/nomatch"
+                className="nav-link py-3 border-bottom"
+                title="NoMatch"
+                data-bs-toggle="tooltip"
+                data-bs-placement="right"
               >
                 {' '}
               </a>
             </li>
             <li></li>
             <a
-              href='/account'
-              class='nav-link py-3 border-bottom'
-              title='Account'
-              data-bs-toggle='tooltip'
-              data-bs-placement='right'
+              href="/account"
+              className="nav-link py-3 border-bottom"
+              title="Account"
+              data-bs-toggle="tooltip"
+              data-bs-placement="right"
             >
               Account
             </a>
           </ul>
         </nav>
-        <main class='col-md-9 ms-sm-auto col-lg-11'>
+        <main className="col-md-9 ms-sm-auto col-lg-11">
           {/* An <Outlet> renders whatever child route is currently active,
             so you can think about this <Outlet> as a placeholder for
             the child routes we defined above. */}
@@ -154,4 +154,4 @@ export const Sidebar = () => (
       </div>
     </div>
   </React.Fragment>
-)
+);
